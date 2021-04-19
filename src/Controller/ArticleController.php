437d96f956e65c7967322c15d81785ca4587baef
kenjy <kenjyzakary@hotmail.com>
2021-04-19 17:34:32 +0200

    update 4

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Images;
 use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -48,11 +50,30 @@ class ArticleController extends AbstractController
 
           if ($form->isSubmitted() && $form->isValid()) {
 
+               $images = $form->get('images')->getData();
+
+               // On boucle sur les images
+               foreach ($images as $image) {
+                    // On génère un nouveau nom de fichier
+                    $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+
+                    // On copie le fichier dans le dossier uploads
+                    $image->move(
+                         $this->getParameter('images_directory'),
+                         $fichier
+                    );
+
+                    // On crée l'image dans la base de données
+                    $img = new Images();
+                    $img->setName($fichier);
+                    $new_article->addImage($img);
+               }
+
                $entityManager = $this->getDoctrine()->getManager();
                $entityManager->persist($new_article);
                $entityManager->flush();
 
-               return $this->redirectToRoute('add-article');
+               return $this->redirectToRoute('affichage-article');
           }
 
           // $this->addFlash("article_add_success", "Votre article a été ajouté avec succès");
@@ -62,9 +83,8 @@ class ArticleController extends AbstractController
      }
 
      /**
-      * @Route("/edit-article/{id}", name="edit-article")
+      * @Route("/{id}", name="edit-article")
       */
-
      public function editArticle($id, Request $request)
      {
           $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
@@ -72,6 +92,26 @@ class ArticleController extends AbstractController
           $form->handleRequest($request);
 
           if ($form->isSubmitted() && $form->isValid()) {
+
+
+               $images = $form->get('images')->getData();
+
+               // On boucle sur les images
+               foreach ($images as $image) {
+                    // On génère un nouveau nom de fichier
+                    $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+
+                    // On copie le fichier dans le dossier uploads
+                    $image->move(
+                         $this->getParameter('images_directory'),
+                         $fichier
+                    );
+
+                    // On crée l'image dans la base de données
+                    $img = new Images();
+                    $img->setName($fichier);
+                    $article->addImage($img);
+               }
 
                $entityManager = $this->getDoctrine()->getManager();
                $entityManager->flush();
@@ -90,7 +130,6 @@ class ArticleController extends AbstractController
      /**
       * @Route("/delete-article/{id}", name="delete-article")
       */
-
      public function deleteArticle($id)
      {
           $entityManager = $this->getDoctrine()->getManager();
@@ -101,5 +140,31 @@ class ArticleController extends AbstractController
 
           $this->addFlash("article_delete_success", "Votre article a été supprimer avec succès");
           return $this->redirectToRoute('affichage-article');
+     }
+
+     /**
+      * @Route("/supprime/image/{id}", name="annonces_delete_image", methods={"DELETE"})
+      */
+     public function deleteImage(Images $image, Request $request)
+     {
+          $data = json_decode($request->getContent(), true);
+
+          // On vérifie si le token est valide
+          if ($this->isCsrfTokenValid('delete' . $image->getId(), $data['_token'])) {
+               // On récupère le nom de l'image
+               $nom = $image->getName();
+               // On supprime le fichier
+               unlink($this->getParameter('images_directory') . '/' . $nom);
+
+               // On supprime l'entrée de la base
+               $em = $this->getDoctrine()->getManager();
+               $em->remove($image);
+               $em->flush();
+
+               // On répond en json
+               return new JsonResponse(['success' => 1]);
+          } else {
+               return new JsonResponse(['error' => 'Token Invalide'], 400);
+          }
      }
 }
